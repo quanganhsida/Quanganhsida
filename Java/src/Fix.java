578a33a8bd216435f7f259c56d9477ae03364773
quanganhsida <72122901+quanganhsida@users.noreply.github.com>
2021-12-07 14:21:19 +0700

    Update #1
    #1

--- Java/src/Fix.java
+++ Java/src/Fix.java
@@ -8,14 +8,21 @@ public class Fix {
         String password = "";
         Scanner sc = new Scanner(System.in);
         try{
+            //1. Get a connection to DTB
             Connection conn = DriverManager.getConnection(url, user, password);
+            
+            //2. Create a statement
             Statement statement = conn.createStatement();
             System.out.println("Nhap cu phap: ");
             String sql1 = sc.nextLine();
             //String sql1 = "INSERT INTO `demo1`(`ID`, `User`, `Password`) VALUES ('2','Quanganhsida','123')";
+            
+            //3. Execute SQL query
             statement.executeUpdate(sql1);
             System.out.println("Insert success !");
             ResultSet rs = statement.executeQuery("select * from demo1");
+            
+            //4. Process the result set
             while (rs.next()){
                 System.out.println(rs.getInt("ID") + " - " + rs.getString("User")
                         + " - " + rs.getString("Password"));
